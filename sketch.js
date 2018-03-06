43e2943af3ce76ab6326032c89ea3960b9a27fc6
Peter Fenoaltea <peter.fenoaltea@f705-05-39315.bmcc.cuny.edu>
2018-03-06 14:39:23 -0500

    Initial commit

---
+++ sketch.js
@@ -0,0 +1,20 @@
+const WIDTH = 470;
+const HEIGHT = 370;
+
+function setup() {
+    createCanvas(WIDTH, HEIGHT);
+    background(200);
+}
+
+function draw() {
+    point(WIDTH / 2, HEIGHT / 2);
+
+}
+function draw() {
+    fill('pink')
+    line(112,269,134,205)
+    line(159,160,134,205)
+    line(93,160,112,269)
+    line(144,94,93,160)
+    
+}
